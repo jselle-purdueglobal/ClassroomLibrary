d87f963b97b9b170cd96e5ab138c7d8412e1e4f5
moose <joseph.sellers06@gmail.com>
2024-06-22 00:13:21 -0600

    Sprint 2 Start

--- LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
+++ LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fd329f0cf272344b59a2403a6cf32108a86097")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
